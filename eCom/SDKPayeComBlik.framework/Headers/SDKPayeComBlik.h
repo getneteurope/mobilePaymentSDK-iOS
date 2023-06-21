1cf9f58a5ee76a597ae6fe76328aefb66fb4b4ed
Peter Stavny <peter.stavny@getneteurope.com>
2023-06-21 13:20:26 +0200

    Release v1.1.0

---
+++ eCom/SDKPayeComBlik.framework/Headers/SDKPayeComBlik.h
@@ -0,0 +1,18 @@
+//
+//  SDKPayeComBlik.h
+//  SDKPayeCom
+//
+//  Created by Marian Polek on 03/10/2022.
+//  Copyright © 2023 PagoNxt Merchant Solutions S.L. and Santander España Merchant Services, Entidad de Pago, S.L.U. You may not use this file except in compliance with the License which is available at https://mit-license.org/
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Blik.
+FOUNDATION_EXPORT double BlikVersionNumber;
+
+//! Project version string for Blik.
+FOUNDATION_EXPORT const unsigned char BlikVersionString[];
+
+#import <SDKPayeComBlik/SDKPayECBlikLevelZeroPayment.h>
+#import <SDKPayeComBlik/SDKPayECBlikRedirectPayment.h>
---
+++ eCom/SDKPayeComRatePay.framework/Headers/SDKPayeComRatePay.h
@@ -0,0 +1,18 @@
+//
+//  SDKPayeComRatePay.h
+//  SDKPayeCom
+//
+//  Created by Marian Polek on 03/10/2022.
+//  Copyright © 2023 PagoNxt Merchant Solutions S.L. and Santander España Merchant Services, Entidad de Pago, S.L.U. You may not use this file except in compliance with the License which is available at https://mit-license.org/
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RatePay.
+FOUNDATION_EXPORT double RatePayVersionNumber;
+
+//! Project version string for RatePay.
+FOUNDATION_EXPORT const unsigned char RatePayVersionString[];
+
+#import <SDKPayeComRatePay/SDKPayECRatePayElvPayment.h>
+#import <SDKPayeComRatePay/SDKPayECRatePayInvoicePayment.h>
