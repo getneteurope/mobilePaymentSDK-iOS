190b7d8fca86ad3a2e4d9606c2662b33242a6a88
PeterStavnyCompleteIt <peter.stavny@external.getneteurope.com>
2023-06-21 10:50:10 +0200

    Release v1.1.0 (#19)
    Co-authored-by: Peter Stavny <peter.stavny@getneteurope.com>

--- eCom/SDKPayeComCardScannerGallery.framework/Headers/SDKPayeComCardScannerGallery.h
+++ eCom/SDKPayeComCardScannerGallery.framework/Headers/SDKPayeComCardScannerGallery.h
@@ -3,7 +3,7 @@
 //  SDKPayeComCardScannerGallery
 //
 //  Created by Vrana, Jozef on 08/01/2019.
-//  Copyright © 2022 PagoNxt Merchant Solutions S.L. and Santander España Merchant Services, Entidad de Pago, S.L.U.  All rights reserved.
+//  Copyright © 2023 PagoNxt Merchant Solutions S.L. and Santander España Merchant Services, Entidad de Pago, S.L.U. You may not use this file except in compliance with the License which is available at https://mit-license.org/
 //
 
 #import <UIKit/UIKit.h>
